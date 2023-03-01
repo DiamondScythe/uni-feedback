775fe4c7cd630b0680e6ce5c0a4b8d4335d07d7d
DiamondScythe <nhutoan399@gmail.com>
2023-03-01 18:34:13 +0700

    init firebase 9 + login (forgot to save some stuff on previous commit)

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,5 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 createApp(App).use(router).mount('#app')
