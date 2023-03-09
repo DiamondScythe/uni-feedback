fd856604dfac9d160586e35c78e1b3968a98d40a
DiamondScythe <nhutoan399@gmail.com>
2023-03-09 22:10:54 +0700

    sqlite3

--- server/src/app.js
+++ server/src/app.js
@@ -4,24 +4,23 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const {sequelize} = require('./models')
-const config = require('./config/config')
+const sqlite3 = require('sqlite3').verbose()
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-require('./routes')(app)
+let sql;
 
-app.post('/post2', (req, res) => {
-    res.send({
-        message: `Hello ${req.body.title}`
-    })
+const db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err)=>{
+    if (err) return console.error(err.message);
+});
+
+// sql = 'CREATE TABLE users(id INTEGER PRIMARY KEY, first_name, last_name, username, password, email)';
+// db.run(sql);
+
+sql = 'INSERT INTO users(first_name,last_name,username,password,email) VALUES (?,?,?,?,?)'
+db.run(sql,["mike", "lol", "diamond", "1as2", "abc@gmail.com"],(err)=>{
+    if (err) return console.error(err.message);
 })
-
-sequelize.sync()
-    .then(() => {
-        app.listen(process.env.Port || 8081)
-        console.log(`Server started on port ${config.port}`)
-    })
