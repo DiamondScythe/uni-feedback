7792dacb20cacd8f70eb78f9964da07f643dc873
DiamondScythe <nhutoan399@gmail.com>
2023-03-24 16:14:39 +0700

    added refresh after idea deletion. Added page system for idea table.

--- src/components/IdeaTable.vue
+++ src/components/IdeaTable.vue
@@ -1,13 +1,11 @@
 <template>
     <table id="ideaTable">
         <tr>
-            <th>id</th>
             <th>idea</th>
             <th>type</th>
             <th>actions</th>
         </tr>
-        <tr v-for="idea in ideas">
-            <th>{{idea.id}}</th>
+        <tr v-for="idea in paginatedItems" :key="idea.id">
             <th>{{idea.title}}</th>
             <th>{{idea.category_id}}</th>
             <th><router-link :to="{ name: 'details', params: { id: idea.id}}">Details</router-link>
@@ -15,6 +13,11 @@
             </th>
         </tr>
     </table>
+
+    <div class="pagination">
+      <button @click="previousPage" :disabled="currentPage === 1">Previous</button>
+      <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+    </div>
 </template>
 
 <script>
@@ -24,7 +27,21 @@ export default {
     data() {
       return {
         ideas: [],
+
+        //pagination settings
+        itemsPerPage: 4,
+        currentPage: 1,
       }
+    },
+    computed: {
+        paginatedItems() {
+            const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+            const endIndex = startIndex + this.itemsPerPage;
+            return this.ideas.slice(startIndex, endIndex);
+        },
+        totalPages() {
+            return Math.ceil(this.ideas.length / this.itemsPerPage);
+        },
     },
     mounted(){
       axios.get("http://localhost:8081/ideas")
@@ -41,9 +58,24 @@ export default {
         },
         deleteItem(id) {
             axios.delete("http://localhost:8081/ideas?id="+id)
-            .then(res => console.log(res))
+            .then(res => {
+                console.log(res);
+
+                return axios.get("http://localhost:8081/ideas")
+            })
+            .then(res => {
+                this.ideas = res.data.ideas;
+            })
             .catch(err => console.log(err))
-        }
+        },
+
+        //these are used for manipulating page count
+        previousPage() {
+        this.currentPage -= 1;
+        },
+        nextPage() {
+        this.currentPage += 1;
+        },
     }
 }
 </script>
