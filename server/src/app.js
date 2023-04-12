b146c1671d875094eb389b5ef4e8c6071e745f8c
DiamondScythe <nhutoan399@gmail.com>
2023-04-12 11:18:53 +0700

    fixed post voting

--- server/src/app.js
+++ server/src/app.js
@@ -1,50 +1,49 @@
-console.log('Hello server')
+console.log("Hello server");
 
-const nodemailer = require('nodemailer');
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
-const sqlite3 = require('sqlite3').verbose()
-require('dotenv').config(); 
+const nodemailer = require("nodemailer");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const sqlite3 = require("sqlite3").verbose();
+require("dotenv").config();
 
-const app = express()
-const fs = require('fs')
-const fastcsv = require('fast-csv');
-const JSZip = require('jszip');
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+const fs = require("fs");
+const fastcsv = require("fast-csv");
+const JSZip = require("jszip");
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
 
 let sql;
 
-const db = require("../db/posts")
-const db2 = require("../db/ideas")
+const db = require("../db/posts");
+const db2 = require("../db/ideas");
 
-app.post("/ideas", async (req, res) =>{
-    const results = await db2.createIdea(req.body);
-    res.status(201).json({id: results[0]});
-})
+app.post("/ideas", async (req, res) => {
+  const results = await db2.createIdea(req.body);
+  res.status(201).json({ id: results[0] });
+});
 
-app.get("/ideas", async (req, res) =>{
-    const results = await db2.getAllIdeas();
-    res.status(200).json({ideas: results});
-})
+app.get("/ideas", async (req, res) => {
+  const results = await db2.getAllIdeas();
+  res.status(200).json({ ideas: results });
+});
 
-app.get("/categories", async (req, res) =>{
-    const results = await db2.getAllCategories();
-    res.status(200).json({categories: results});
-})
-
+app.get("/categories", async (req, res) => {
+  const results = await db2.getAllCategories();
+  res.status(200).json({ categories: results });
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.post('/email', async (req, res) => {
+app.post("/email", async (req, res) => {
   try {
     const { email } = req.body; // destructed email value from request
     let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+      host: "smtp.gmail.com",
       port: 587,
       secure: false,
       auth: {
@@ -55,51 +54,52 @@ app.post('/email', async (req, res) => {
 
     const msg = {
       from: `"Khuong" <${process.env.MAIL_USER}>`,
-      to: 'khuongwhitelily@gmail.com',
-      subject: 'Hello',
-      html: '<b>Hello world?</b>',
+      to: "khuongwhitelily@gmail.com",
+      subject: "Hello",
+      html: "<b>Hello world?</b>",
     };
 
     const info = await transporter.sendMail(msg);
 
-    console.log('Message sent: %s', info.messageId);
+    console.log("Message sent: %s", info.messageId);
 
-    res.send('Email Sent!');
-    
+    res.send("Email Sent!");
   } catch (err) {
     console.error(err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
-
-
-
-app.get('/download', async (req, res) => {
+app.get("/download", async (req, res) => {
   // Lấy dữ liệu từ database và chuyển đổi sang định dạng CSV
-  await db2.getAllIdeas().then(async (data) => {
-    // Tạo file CSV từ dữ liệu lấy được
-    const csvStream = fastcsv.format({ headers: true });
-    csvStream.pipe(fs.createWriteStream('data.csv')).on('finish', async () => {
-      // Nén file CSV thành file zip
-      const zip = new JSZip();
-      const dataFile = await fs.promises.readFile('data.csv');
-      zip.file('data.csv', dataFile);
-      const zipContent = await zip.generateAsync({ type: 'nodebuffer' });
+  await db2
+    .getAllIdeas()
+    .then(async (data) => {
+      // Tạo file CSV từ dữ liệu lấy được
+      const csvStream = fastcsv.format({ headers: true });
+      csvStream
+        .pipe(fs.createWriteStream("data.csv"))
+        .on("finish", async () => {
+          // Nén file CSV thành file zip
+          const zip = new JSZip();
+          const dataFile = await fs.promises.readFile("data.csv");
+          zip.file("data.csv", dataFile);
+          const zipContent = await zip.generateAsync({ type: "nodebuffer" });
 
-      // Trả về file zip để tải xuống
-      res.set('Content-Disposition', 'attachment; filename=data.zip');
-      res.set('Content-Type', 'application/zip');
-      res.status(200).send(zipContent);
+          // Trả về file zip để tải xuống
+          res.set("Content-Disposition", "attachment; filename=data.zip");
+          res.set("Content-Type", "application/zip");
+          res.status(200).send(zipContent);
+        });
+      data.forEach((row) => {
+        csvStream.write(row);
+      });
+      csvStream.end();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
     });
-    data.forEach((row) => {
-      csvStream.write(row);
-    });
-    csvStream.end();
-  }).catch((err) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  });
 });
 
 //get idea details
@@ -107,32 +107,46 @@ app.get('/download', async (req, res) => {
 //this will get details of post at id = 5
 //you can add multiple params like this:
 //  /details?param1=value1&param2=value2
-app.get('/details', async (req, res) => {
-    const { id } = req.query;
-    const results = await db2.getIdeaDetails(id);
-    if (results.length > 0) {
-        res.status(200).json(results[0]);
-      } else {
-        res.status(404).json({ message: 'No idea found with that ID' });
-      }
-  });
+app.get("/details", async (req, res) => {
+  const { id } = req.query;
+  const results = await db2.getIdeaDetails(id);
+  if (results.length > 0) {
+    res.status(200).json(results[0]);
+  } else {
+    res.status(404).json({ message: "No idea found with that ID" });
+  }
+});
 
 //post comments
-app.post('/comments', async (req, res) => {
-    const results = await db2.createComment(req.body)
-    res.status(201)
-})
+app.post("/comments", async (req, res) => {
+  const results = await db2.createComment(req.body);
+  res.status(201);
+});
 
 //get comments from idea id
-app.get('/comments', async (req, res) => {
-    const { ideaId } = req.query;
-    const results = await db2.getComments(ideaId);
-    if (results.length > 0) {
-        res.status(200).json(results);
-      } else {
-        res.status(404).json({ message: 'No comments found with that idea ID' });
-      }
-})
+app.get("/comments", async (req, res) => {
+  const { ideaId } = req.query;
+  const results = await db2.getComments(ideaId);
+  if (results.length > 0) {
+    res.status(200).json(results);
+  } else {
+    res.status(404).json({ message: "No comments found with that idea ID" });
+  }
+});
 
+//voting
+
+app.post("/voteIdea", async (req, res) => {
+  const results = await db2.voteIdea(req.body);
+  res.status(201);
+});
+
+app.get("/ideavotes", async (req, res) => {
+  const { id } = req.query;
+  console.log(id);
+  const results = await db2.getAllIdeaVotes(id);
+  console.log(results);
+  res.status(200).json(results[0]);
+});
 
 app.listen(8081, () => console.log("server is now running on port 8081"));
--- src/components/PostVoting.vue
+++ src/components/PostVoting.vue
@@ -1,75 +1,71 @@
 <template>
-<button @click="upvoteAction">
-    Upvote
-</button>
-<button @click="middleVoteAction">
-    Neutral vote
-</button>
-<button @click="downvoteAction">
-    Downvote
-</button>
-Total votes: {{ totalVotes }}
+  <button @click="upvoteAction">Upvote</button>
+  <button @click="middleVoteAction">Neutral vote</button>
+  <button @click="downvoteAction">Downvote</button>
+  Total votes: {{ totalVotes }}
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    props: ['idea_id', 'user_id'],
-    data() {
-        return{
-            totalVotes: 0,
-        }
+  props: ["idea_id", "user_id"],
+  data() {
+    return {
+      totalVotes: 0,
+    };
+  },
+
+  mounted() {
+    this.getVotes();
+  },
+
+  methods: {
+    upvoteAction() {
+      axios
+        .post("http://localhost:8081/voteIdea", {
+          value: "1",
+          user_id: this.user_id,
+          idea_id: this.idea_id,
+        })
+        .catch((err) => console.log(err));
+      this.getVotes();
     },
-    
-    mounted() {
-        this.getVotes()
+    downvoteAction() {
+      axios
+        .post("http://localhost:8081/voteIdea", {
+          value: "-1",
+          user_id: this.user_id,
+          idea_id: this.idea_id,
+        })
+        .catch((err) => console.log(err));
+      this.getVotes();
     },
 
-    methods: {
-        upvoteAction(){
-            axios.post('http://localhost:8081/voteIdea', {
-                value: '1',
-                user_id: this.user_id,
-                idea_id: this.idea_id,
-            })
-            .catch(err => console.log(err));
-            this.getVotes()
-        },
-        downvoteAction(){
-            axios.post('http://localhost:8081/voteIdea', {
-                value: '-1',
-                user_id: this.user_id,
-                idea_id: this.idea_id,
-            })
-            .catch(err => console.log(err));
-            this.getVotes()
-        },
+    //this should fire when the user "unselects" a vote
+    middleVoteAction() {
+      axios
+        .post("http://localhost:8081/voteIdea", {
+          value: "0",
+          user_id: this.user_id,
+          idea_id: this.idea_id,
+        })
+        .catch((err) => console.log(err));
+      this.getVotes();
+    },
 
-        //this should fire when the user "unselects" a vote
-        middleVoteAction(){
-            axios.post('http://localhost:8081/voteIdea', {
-                value: '0',
-                user_id: this.user_id,
-                idea_id: this.idea_id,
-            })
-            .catch(err => console.log(err));
-            this.getVotes()
-        },
-
-        getVotes(){
-            axios.get("http://localhost:8081/ideavotes?id="+this.idea_id)
-            .then(res => {
-                if (res.data.votes !== null){
-                    this.totalVotes = res.data.votes;
-                }
-            }
-            )
-        }
-    }
-}
+    getVotes() {
+      axios
+        .get("http://localhost:8081/ideavotes?id=" + this.idea_id)
+        .then((res) => {
+          console.log(res.data.votes);
+          if (res.data.votes !== null) {
+            this.totalVotes = res.data.votes;
+          }
+        });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
