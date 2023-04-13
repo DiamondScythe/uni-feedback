d55d7d906e7229642ec0710bd6373405ebc0029d
DiamondScythe <nhutoan399@gmail.com>
2023-04-13 18:20:21 +0700

    basic admin dashboard layout

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -1,3 +1,35 @@
-<template>This is admin dashboard</template>
+<template>
+  <div class="container">
+    <div class="sidebar">
+      <!-- List of links for the sidebar -->
+      <ul>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+      </ul>
+    </div>
+    <div class="content">
+      <!-- Your components will go here -->
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
 
-<script></script>
+<style>
+.container {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+}
+
+.sidebar {
+  width: 20%;
+  background-color: #f1f1f1;
+  padding: 20px;
+}
+
+.content {
+  width: 80%;
+  padding: 20px;
+}
+</style>
