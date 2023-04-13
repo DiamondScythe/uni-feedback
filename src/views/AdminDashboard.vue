01f4c2c5d74dc219a54447f3e6f88e96a9055a61
DiamondScythe <nhutoan399@gmail.com>
2023-04-13 22:31:03 +0700

    added chartjs. added chart to idea management page. will move chart to its own component soon

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -5,24 +5,27 @@
       <ul>
         <li><a href="#" @click="categoryOn">Category Management</a></li>
         <li><a href="#" @click="staffOn">Staff Management</a></li>
+        <li><a href="#" @click="ideaOn">Idea Management</a></li>
       </ul>
     </div>
     <div class="content">
       <!-- Content goes here -->
       <CategoryManagement v-if="categoryShow"></CategoryManagement>
       <StaffManagement v-if="staffShow"></StaffManagement>
+      <IdeaManagement v-if="ideaShow"></IdeaManagement>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 import CategoryManagement from "../components/Admin/CategoryManagement.vue";
 import StaffManagement from "../components/Admin/StaffManagement.vue";
+import IdeaManagement from "../components/Admin/IdeaManagement.vue";
 
 export default {
   data() {
     return {
+      ideaShow: false,
       categoryShow: true,
       staffShow: false,
     };
@@ -30,15 +33,23 @@ export default {
   components: {
     CategoryManagement,
     StaffManagement,
+    IdeaManagement,
   },
   methods: {
     categoryOn() {
       this.categoryShow = true;
       this.staffShow = false;
+      this.ideaShow = false;
     },
     staffOn() {
       this.categoryShow = false;
       this.staffShow = true;
+      this.ideaShow = false;
+    },
+    ideaOn() {
+      this.categoryShow = false;
+      this.staffShow = false;
+      this.ideaShow = true;
     },
   },
 };
