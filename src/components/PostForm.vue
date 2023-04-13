26175aabf6f3ca2d0e4197687f20956f65f767a1
DiamondScythe <nhutoan399@gmail.com>
2023-04-14 00:34:41 +0700

    removed unnecessary info on post form

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,63 +1,62 @@
 <template>
-    <form @submit.prevent="handleSubmit">
-        <br>
+  <form @submit.prevent="handleSubmit">
+    <br />
 
-        <label>Post title </label>
-        <input type="text" required v-model="title">
-        <br>
+    <label>Post title </label>
+    <input type="text" required v-model="title" />
+    <br />
 
-        <label>Post body </label>
-        <input type="text" required v-model="body">
-        <br>
+    <label>Post body </label>
+    <input type="text" required v-model="body" />
+    <br />
 
-        <label for="category-select">Select a category:</label>
-        <select id="category-select" v-model="selectedCategory">
-        <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
-        </select>
+    <label for="category-select">Select a category:</label>
+    <select id="category-select" v-model="selectedCategory">
+      <option
+        v-for="category in categories"
+        :key="category.id"
+        :value="category.id"
+      >
+        {{ category.name }}
+      </option>
+    </select>
 
-        <button>Post</button>
-    </form>
-
-    <p>Title: {{ title }}</p>
-    <p>Body: {{ body }}</p>
-    <p>Selected: {{ selectedCategory }}</p>
+    <button>Post</button>
+  </form>
 </template>
 
 <script>
-import axios from  'axios';
+import axios from "axios";
 
 export default {
-    data(){
-        return{
-            title: '',
-            body: '',
-            user_id: '1',
-            categories: [],
-            selectedCategory: ''
-        }
+  data() {
+    return {
+      title: "",
+      body: "",
+      user_id: "1",
+      categories: [],
+      selectedCategory: "",
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:8081/categories").then((res) => {
+      this.categories = res.data.categories;
+    });
+  },
+  methods: {
+    handleSubmit() {
+      axios
+        .post("http://localhost:8081/ideas", {
+          title: this.title,
+          body: this.body,
+          user_id: this.user_id,
+          category_id: this.selectedCategory,
+        })
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
     },
-    mounted(){
-        axios.get("http://localhost:8081/categories")
-            .then(res => {
-                this.categories = res.data.categories;
-            }
-            )
-    },
-    methods: {
-        handleSubmit(){
-            axios.post('http://localhost:8081/ideas',{
-                title: this.title,
-                body: this.body,
-                user_id: this.user_id,
-                category_id: this.selectedCategory,
-            })
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
-        }
-    }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
