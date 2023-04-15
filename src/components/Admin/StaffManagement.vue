adeede2d768cfe0b08023f49cf91f4d0c2f8eb54
DiamondScythe <nhutoan399@gmail.com>
2023-04-15 21:40:41 +0700

    added staff to staff management page

--- src/components/Admin/StaffManagement.vue
+++ src/components/Admin/StaffManagement.vue
@@ -3,13 +3,11 @@
   <br />
   <table id="staffTable">
     <tr>
-      <th>Name</th>
       <th>Email</th>
       <th>Role</th>
       <th>Actions</th>
     </tr>
     <tr v-for="employee in staff" :key="employee.id">
-      <td>{{ employee.name }}</td>
       <td>{{ employee.email }}</td>
       <td>{{ employee.role }}</td>
       <td>
@@ -26,21 +24,19 @@ import axios from "axios";
 export default {
   data() {
     return {
-      staff: [
-        {
-          name: "John Doe",
-          email: "email@gmail.com",
-          role: "Admin",
-        },
-        {
-          name: "Jane Doe",
-          email: "email.gmail.com",
-          role: "QA",
-        },
-      ],
+      staff: [],
     };
   },
   components: {},
-  mounted() {},
+  mounted() {
+    axios
+      .get("http://localhost:8081/getAllStaff")
+      .then((res) => {
+        this.staff = res.data;
+      })
+      .catch((err) => {
+        console.log(error);
+      });
+  },
 };
 </script>
